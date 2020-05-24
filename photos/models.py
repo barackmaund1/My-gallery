5d0c885f0a394b21895f142dead65d562b9d0de6
barackmaund1 <pmaundu49@gmail.co>
2020-05-24 12:18:26 +0300

    created category test class

--- photos/models.py
+++ photos/models.py
@@ -2,32 +2,58 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    id=models.AutoField
     name=models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
     def save_location(self):
         self.save()
-    def get_loca  
-
+    def delete_location(self) :
+        self.delete() 
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(location=value)    
+      
+    def display_locations()
 class Category(models.Model) :
-    id=models.AutoField
     name=models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+    def delete_category(self):
+        self.delete()   
 
     def __str__(self):
         return self.name        
 class Image(models.Model):
-    id=models.AutoField
     image_name=models.CharField(max_length=50)
     image_description=models.CharField(max_length=250)
     image_location=models.ForeignKey(Location,on_delete=models.CASCADE)
     image_category=models.ForeignKey(Category,on_delete=models.CASCADE)
     image=models.ImageField(upload_to= 'images/')
+    post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.image_name
 
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images    
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()        
+        
+class Meta:
+    ordering = ['date']
 
 
 
--- photos/tests.py
+++ photos/tests.py
@@ -5,7 +5,7 @@ class LocationTestClass(TestCase):
     # 'Set up method to run before every test'
 
     def setUp(self):
-        self.location=Location(name='Nairobi')
+        self.location=Location(id=1,name='Nairobi')
         self.location.save()
     def tearDown(self):
         self.location.delete()    
@@ -15,8 +15,36 @@ class LocationTestClass(TestCase):
     def test_save_location(self):
         self.location.save_location()
         locations=Location.objects.all()
-        self.assertTrue(len(locations)>0)
+        self.assertEqual(len(locations),1)
     def test_get_location(self):
         location=Location.objects.get(id=1)
         self.assertEquals(location.name,'Nairobi')
-      
+    def test_delete_location(self) :
+        self.location.delete()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)==0)
+    def test_update_location(self):
+        new_location='makueni'
+        self.location.update_location(self.location.id,new_location)
+        new_location=Location.objects.filter(name='makueni')
+        self.assertTrue(len(new_location)>0)
+
+class CategoryTestClass(TestCase):
+    def setUp(self) :
+        self.category=Category(id=1,name='food') 
+        self.category.save() 
+    def tearDown(self):
+
+        self.category.delete()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+    def test_save_category(self):
+        self.category.save_category()
+        category=Category.objects.all()
+        self.assertTrue(len(category)>0)
+    def test_delete_category(self):
+        self.category.delete() 
+        category=Category.objects.all(id=1)
+        self.assertTrue(len(category)==0)
+    def test_update_category(self):
+        self.category.save_category()
